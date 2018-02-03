6505211c5b61aa38ee94dcdb80d7f26ad822ea90
Edward Hambleton <ehambleton@me.com>
2018-02-03 17:15:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -92,10 +92,11 @@ console.log(index);
 
 function placeOrder(cardNumber) {
   var creditCardNumber = cardNumber
+  var totalPrice = 0
 if (cardNumber === undefined) {
   console.log ("Sorry, we don\'t have a credit card on file for you.")
 } else {
-  total()
+  var totalPrice = total()
   console.log (`Your total cost is $${totalPrice}, which will be charged to ${creditCardNumber}.`);
   cart = []
 }
